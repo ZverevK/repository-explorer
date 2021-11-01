8d0ed8f6bb5b51eb940da1b6974e70b68af9efb4
Zverev Konstantin <zverev.a.konstantin@gmail.com>
2021-11-01 23:38:16 +0300

    init

--- src/components/UI/MyButton.vue
+++ src/components/UI/MyButton.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="">
-  </div>
+
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-};
+  name: 'my-button',
+}
 </script>
 
-<style>
+<style scoped>
+
 </style>
--- src/components/UI/MyInput.vue
+++ src/components/UI/MyInput.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="">
-  </div>
+
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-};
+  name: 'my-input',
+}
 </script>
 
-<style>
+<style scoped>
+
 </style>
